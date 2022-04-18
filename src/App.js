de994b770a16c0564a1a4ef31aeecad84e52c81c
Victoria123654 <krasikova.vg@gmail.com>
2022-04-17 20:23:01 -0700

    app Weather

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchEngine from "./SearchEngine"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Weather App</h1>
+        <br />
+        <SearchEngine />
+      </div>
     </div>
   );
 }
